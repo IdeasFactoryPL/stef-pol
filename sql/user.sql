bbcf469c795824652c83d08aa3949b588770a05a
Rafał Petryka <rafalpetryka@gmail.com>
2015-07-10 21:33:19 +0200

    Added sql files

---
+++ sql/user.sql
@@ -0,0 +1,12 @@
+# Uprawnienia dla `Dan`@`localhost`
+
+GRANT ALL PRIVILEGES ON *.* TO 'Dan'@'localhost' IDENTIFIED BY PASSWORD '*0EA1A62162186CC6EEEC865212001FCAAA39FFF9' WITH GRANT OPTION;
+
+GRANT ALL PRIVILEGES ON `eco`.* TO 'Dan'@'localhost';
+
+
+# Uprawnienia dla `admin`@`localhost`
+
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' IDENTIFIED BY PASSWORD '*00A51F3F48415C7D4E8908980D443C29C69B60C9' WITH GRANT OPTION;
+
+GRANT ALL PRIVILEGES ON `blue_leopard`.* TO 'admin'@'localhost';
