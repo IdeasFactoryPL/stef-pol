8f224eb9b02e71117dafa52b566881e47bf4b7a2
UmiKuroi <umikuroi@gmail.com>
2015-07-13 12:56:11 +0200

    Footer first concept

--- wp-content/themes/StefPol/footer.php
+++ wp-content/themes/StefPol/footer.php
@@ -15,6 +15,14 @@
 </div> <!-- #wrap -->
 
 <footer>
+	<div id="footer">
+		<div class="container footer-nav ">
+			<div class="footer-navigtion">
+				<?php wp_nav_menu( array( 'theme_location'=> 'footer-menu', 'menu_class' => 'list-inline', 'depth' =>1, 'container' => false, 'fallback_cb' => false ) ); ?>
+			</div>
+
+		</div>
+	</div>
 		<?php
 		/* A sidebar in the footer? Yep. You can customize
 		 * your footer with three columns of widgets.
@@ -24,19 +32,7 @@
 			$copyrighttxt = ! empty($copyrighttxt) ? $copyrighttxt : __('&copy; ', 'openstrap') . __(date('Y')) . sprintf(' <a href="%1$s" title="%2$s" rel="home">%3$s</a>', esc_url(home_url( '/' )), get_bloginfo( 'name' ), get_bloginfo( 'name' ));
 			$d=sprintf('<a href="%1$s" title="%2$s" rel="home">%3$s</a>', esc_url(home_url( '/' )), get_bloginfo( 'name' ), get_bloginfo( 'name' ));			
 		?>
-<div id="footer">
-  <div class="container footer-nav ">	
-	<div class="pull-left">
-	<?php 
-		wp_nav_menu( array( 'theme_location' => 'footer-menu', 'menu_class' => 'list-inline', 'depth' =>1, 'container' => false, 'fallback_cb' => false ) ); 
-	?>
-	</div>	
 
-	<div class="pull-right hidden-xs">
-
-	</div> 	
-  </div>
-</div>
 </footer> 
 </div> <!-- #bodychild -->
 <?php wp_footer(); ?>
